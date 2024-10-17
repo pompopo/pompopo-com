a6d0d995dd5ce195b4d7fd4d251f526c2117e235
pompopo <pompopo@gmail.com>
2024-10-17 17:36:19 +0900

    add github action to deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
 
   // Set the production url of your site here
   url: "https://pompopo.com",
+  // GitHub
+  organizationName: "pompopo",
+  projectName: "pompopo-com",
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
